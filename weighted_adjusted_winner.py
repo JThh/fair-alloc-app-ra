3a384541daccb5f67437c7b763f33b01c596e67e
JThh <julius.han@outlook.com>
2023-06-02 02:40:54 +0800

    Add editable dataframes; add wef(x,1-x) algo; add preference uploading and saving option; add session state; remove checker

--- weighted_adjusted_winner.py
+++ weighted_adjusted_winner.py
@@ -1,3 +1,4 @@
+import base64
 from collections import defaultdict
 from functools import lru_cache
 import json
@@ -48,21 +49,35 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-@lru_cache
-def generate_preferences(n, m):
-    return np.random.rand(n, m)
+@st.cache_data
+def load_preferences(m, n, upload_preferences):
+    if upload_preferences:
+        preferences_default = None
+        # Load the user-uploaded preferences file
+        try:
+            preferences_default = pd.read_csv(upload_preferences)
+            if preferences_default.shape != (n, m):
+                st.error(f"The uploaded preferences file should have a shape of ({n}, {m}).")
+                st.stop()
+        except Exception as e:
+            st.error("An error occurred while loading the preferences file.")
+            st.stop()
+    else:
+        preferences_default = pd.DataFrame(np.random.randint(1, 100, (n, m)), columns=[f"Item {i+1}" for i in range(m)], 
+                                            index=[f"Agent {i+1}" for i in range(n)])
+    return preferences_default
 
-def wef1_po_algorithm(m, n, weights, preferences):
-    # Implementation of WEF1+PO algorithm
-    # Add your code here
-    objects = sorted(list(range(m)), key=lambda x:preferences[0][x] / preferences[1][x], reverse=True)
-    d = 1
-    while sum([preferences[0][objects[i]] for i in range(d+1)]) / weights[0] \
-            < sum([preferences[0][objects[j]] for j in range(d+2, m)]) / weights[1]:
-        d += 1
-    return {0: objects[:d+1], 1:  objects[d+1:]}
+# def wef1_po_algorithm(x, m, n, weights, preferences):
+#     # Implementation of WEF1+PO algorithm
+#     # Add your code here
+#     objects = sorted(list(range(m)), key=lambda x:preferences[0][x] / preferences[1][x], reverse=True)
+#     d = 1
+#     while sum([preferences[0][objects[i]] for i in range(d+1)]) / weights[0] \
+#             < sum([preferences[0][objects[j]] for j in range(d+2, m)]) / weights[1]:
+#         d += 1
+#     return {0: objects[:d+1], 1:  objects[d+1:]}
 
-def wef1_algorithm(m, n, weights, preferences):
+def wef1x_algorithm(x, m, n, weights, preferences):
     # Implementation of WEF1 algorithm
     # Add your code here
     bundles = defaultdict(list)
@@ -70,7 +85,7 @@ def wef1_algorithm(m, n, weights, preferences):
     remaining_items = list(range(m))
     # print(bundles, times, remaining_items)
     while remaining_items:
-        i = np.argmin(times / weights)
+        i = np.argmin((times + (1 - x)) / weights)
         o = remaining_items[np.argmax(preferences[i][remaining_items])]
         # Add o to bundle A_i
         bundles[i].append(o)
@@ -79,25 +94,21 @@ def wef1_algorithm(m, n, weights, preferences):
         times[i] += 1
     return bundles
 
-def wef1_checker(outcomes, m, n, weights, preferences):
+def wef1x_checker(outcomes, x, m, n, weights, preferences):
     # Implementation of WEF1 checker
     # Add your code here
-    print(outcomes)
-    def is_single_wef1(i, j, bundle_i, bundle_j):
-        if sum(preferences[i][bundle_i]) / weights[i] < \
-                sum(preferences[i][bundle_j]) / weights[j]:
-            most_valuable_item_value = max(preferences[i][bundle_j])
-            return sum(preferences[i][bundle_i]) / weights[i] >= \
-                (sum(preferences[i][bundle_j]) - most_valuable_item_value) / weights[j]
-        return True
-
+    y = 1 - x
+    def is_single_wef1x(i, j, bundle_i, bundle_j):
+        left = sum(preferences[i][bundle_j]) / weights[j] - sum(preferences[i][bundle_i]) / weights[i]
+        right = (y / weights[i] + x / weights[j]) * max(preferences[i][bundle_j])
+        return left <= right
     for i in range(n):
         for j in range(i, n):
             bi, bj = outcomes[i][1], outcomes[j][1]
-            if not is_single_wef1(i, j, bi, bj) or not is_single_wef1(j, i, bj, bi):
-                st.write("Not fulfiling WEF1.")
+            if not is_single_wef1x(i, j, bi, bj):
+                st.write(f"Not fulfiling WEF({x}, {1-x}).")
                 return
-    st.write("Fulfiled WEF1.")
+    st.write(f"Fulfiled WEF({x}, {1-x}).")
     return
 
 # Set the title and layout of the web application
@@ -153,35 +164,42 @@ st.sidebar.markdown(
 
 
 # Add input components
-col1, col2 = st.columns(2)
-m = col1.number_input("Number of indivisible goods (m)", min_value=2, max_value=1000, value=10, step=1)
-n = col2.number_input("Number of agents (n)", min_value=2, max_value=1000, step=1)
+col1, col2, col3 = st.columns(3)
+m = col1.number_input("Number of goods (m)", min_value=2, value=10, step=1)
+n = col2.number_input("Number of agents (n)", min_value=2, step=1)
+x = col3.slider("Choose a value for x in WEF(x,1-x)", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+
+upload_preferences = st.file_uploader(f"Upload Preferences of shape ({n}, {m})", type=['csv'])
 
 if m < n:
     st.warning("The number of goods (m) must be equal to or greater than the number of agents (n).")
 else:
     weights = np.arange(1, n+1)
-    preferences = generate_preferences(n, m)
-
+    
     st.write("Agent Preferences:")
-    st.dataframe(preferences)
+    preferences = load_preferences(m, n, upload_preferences)
+    edited_prefs = st.experimental_data_editor(preferences, key="data_editor")
+    # Convert to numpy arrays
+    preferences = edited_prefs.values
+    
+    # Download preferences as CSV
+    preferences_csv = edited_prefs.to_csv(index=False)
+    b64 = base64.b64encode(preferences_csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="preferences.csv">Download Preferences CSV</a>'
+    st.markdown(href, unsafe_allow_html=True)
 
-    st.write("🔍 Running the Algorithm... ⏳")
+    st.write("⏳ Run the Weight-Adjusted Picker (WEF(x,1-x))...")
+    
+    with st.spinner('Executing...'):
+        time.sleep(n * m * 0.01)    
+    # for i in range(50):
+    #     time.sleep(0.025)
+    #     progress_bar.progress(i / 50)
 
-    if n == 2:
-        st.write("Running Weighted Adjust Winner (WEF1+PO) algorithm...")
-        # with st.beta_expander("The Algorithm"):
-        #     st.write("")
-        start_time = time.time()
-        outcomes = wef1_po_algorithm(m, n, weights, preferences)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-    else:
-        st.write("Pick the Least Weight-Adjusted Frequent Picker (WEF1)...")
-        start_time = time.time()
-        outcomes = wef1_algorithm(m, n, weights, preferences)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
+    start_time = time.time()
+    outcomes = wef1x_algorithm(x, m, n, weights, preferences)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 
     st.write("🎉 Outcomes:")
     outcomes = [[key, sorted(value)] for key, value in outcomes.items()]
@@ -193,23 +211,22 @@ else:
     st.write("⏱️ Timing Results:")
     st.write(f"Elapsed Time: {elapsed_time:.4f} seconds")
 
-    # for agent_id, item_list in outcomes.items():
-    #     st.write(f"{agent_id}: {sorted(item_list)}")
+    # st.warning("⚠️ Running the WEF Checker takes exponential time!")
 
-    st.warning("⚠️ Running the WEF1 Checker takes exponential time!")
-
-    if st.button("Run WEF1 Checker"):
-        st.write("Running WEF1 Checker...")
-        wef1_checker(outcomes, m, n, weights, preferences)
-        st.write("WEF1 Checker completed.")
+    # if st.button(f"Run WEF({x}, {1-x}) Checker"):
+    #     st.write(f"Running WEF Checker...")
+    #     wef1x_checker(outcomes, x, m, n, weights, preferences)
         
     # Download outcomes in JSON format
     outcomes_json = json.dumps([oc[1] for oc in outcomes], indent=4)
     st.markdown("### Download Outcomes as JSON")
-    st.download_button(
-        label="Download JSON",
-        file_name="outcomes.json",
-        mime="application/json",
-        data=outcomes_json,
-    )
+    b64 = base64.b64encode(outcomes_json.encode()).decode()
+    href = f'<a href="data:application/json;base64,{b64}" download="outcomes.json">Download Outcomes JSON</a>'
+    st.markdown(href, unsafe_allow_html=True)
     st.json(outcomes_json)
+    # st.download_button(
+    #     label="Download Allocations",
+    #     file_name="outcomes.json",
+    #     mime="application/json",
+    #     data=outcomes_json,
+    # )
