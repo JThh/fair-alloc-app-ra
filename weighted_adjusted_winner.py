97b008145d45379a9d8df18280ece2f8cf45ac45
JThh <julius.han@outlook.com>
2023-06-03 00:43:19 +0800

    Temporarily enable menu

--- weighted_adjusted_winner.py
+++ weighted_adjusted_winner.py
@@ -375,12 +375,12 @@ else:
 
         # footer {visibility: hidden;}
 
-hide_streamlit_style = """
-    <style>
-        #MainMenu {visibility: hidden;}
-    </style>
-"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+# hide_streamlit_style = """
+#     <style>
+#         #MainMenu {visibility: hidden;}
+#     </style>
+# """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 st.markdown(
     """
