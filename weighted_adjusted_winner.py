ef00ca1dafbbf7cdc7a6049b2e92264a8752e453
JThh <julius.han@outlook.com>
2023-06-06 13:32:17 +0800

    Add celebratory balloons

--- weighted_adjusted_winner.py
+++ weighted_adjusted_winner.py
@@ -347,7 +347,7 @@ href = f'<a href="data:file/csv;base64,{b64}" download="weights.csv">Download We
 st.markdown(href, unsafe_allow_html=True)
 
 # Agent Preferences
-st.write("📊 Agent Preferences (0-1000, copyable from local sheets):")
+st.write("📊 Agent Preferences (0-1000, support Excel behaviors):")
 preferences = load_preferences(m, n, upload_preferences)
 # for col in preferences.columns:
 #     preferences[col] = preferences[col].map(str)
