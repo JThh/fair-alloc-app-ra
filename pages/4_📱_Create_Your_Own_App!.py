1c4310062dc71c0e2b24db627d5165da9145f995
JThh <julius.han@outlook.com>
2023-06-20 23:26:32 +0800

    Make advanced contribution guide

--- pages/4_📱_Create_Your_Own_App!.py
+++ pages/4_📱_Create_Your_Own_App!.py
@@ -146,7 +146,7 @@ def main():
 
         st.markdown("Alternatively, if you are an experienced programmer, you can refer to this <a href='https://github.com/JThh/fair-alloc-app-ra/blob/new_main/contribution/ADVANCED_CONTRIBUTION.md'>advanced guide</a> for implementing more complex functionalities.", unsafe_allow_html=True)
         
-        st.markdown("If you wish to publish your app on this site (https://fair-alloc.streamlit.app), please email us. Make sure to include any **paper references** that are relevant, and optionally, you can include **your code** in the email body.<br><br>", unsafe_allow_html=True)
+        st.markdown("**If you wish to publish your app on this site (https://fair-alloc.streamlit.app), please email us.** Make sure to include any **paper references** that are relevant, and optionally, you can include **your code** in the email body.<br><br>", unsafe_allow_html=True)
         # Email button
         # st.markdown("### Email your codes to the developer team")
         subject = f"Generated Code - {algorithm_name}"
